3a214cbcd16955abc79be6981bf80ce803ce0028
abrarShariar <shariarabrar@gmail.com>
2016-09-04 16:06:35 +0600

    Karatsuba multification | FIX for odd numbers

---
+++ karatsuba/karatsuba_101.cpp
@@ -0,0 +1,51 @@
+/*
+    testing Karatsuba multiplication algorithm (works for n=1,n=2,n=4
+
+    -- FIX FOR ODD NUMBERS AND {n}+6
+*/
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+long int getNDigit(long int,long int);
+long int getKaratsuba(long int,long int,long int);
+
+int main(){
+
+    long int num_x=222323;
+    long int num_y=111122;
+    long int len=6;
+
+    long int fx1=getNDigit(num_x,len/2);
+    long int fx2=num_x-fx1*pow(10,len/2);
+    //cout<<fx1<<" "<<fx2<<endl;
+
+    cout<<getKaratsuba(num_x,num_y,len)<<endl;
+}
+
+//karatsuba method
+long int getKaratsuba(long int X,long int Y,long int n){
+
+    if(n<=1){
+        return X*Y;
+    }
+    long int a=getNDigit(X,n/2);
+    long int b=X-a*pow(10,n/2);
+    long int c=getNDigit(Y,n/2);
+    long int d=Y-c*pow(10,n/2);
+
+    //cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl;
+    long int product=(pow(10,n)*getKaratsuba(a,c,n/2)) + (pow(10,n/2)*(getKaratsuba(a,d,n/2)+getKaratsuba(b,c,n/2))) + getKaratsuba(b,d,n/2);
+    return product;
+}
+
+//get N-digits ommited number as passed by len
+long int getNDigit(long int num,long int limit){
+    for(long int i=0;i<limit;i++){
+        num=num/10;
+    }
+    return num;
+}
+
+
