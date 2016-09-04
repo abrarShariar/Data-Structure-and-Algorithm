40fb406ae463e587707d73d3592454db527a2b98
abrarShariar <shariarabrar@gmail.com>
2016-09-04 17:26:39 +0600

    karatsuba multiplication optimized with 3 recursive call | FIX FOR ODD

--- karatsuba/karatsuba_101.cpp
+++ karatsuba/karatsuba_101.cpp
@@ -1,7 +1,7 @@
 /*
     testing Karatsuba multiplication algorithm (works for n=1,n=2,n=4
-
     -- FIX FOR ODD NUMBERS AND {n}+6
+
 */
 
 #include<iostream>
@@ -13,8 +13,8 @@ long int getKaratsuba(long int,long int,long int);
 
 int main(){
 
-    long int num_x=222323;
-    long int num_y=111122;
+    long int num_x=1234;
+    long int num_y=5678;
     long int len=6;
 
     long int fx1=getNDigit(num_x,len/2);
@@ -35,8 +35,14 @@ long int getKaratsuba(long int X,long int Y,long int n){
     long int c=getNDigit(Y,n/2);
     long int d=Y-c*pow(10,n/2);
 
-    //cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl;
-    long int product=(pow(10,n)*getKaratsuba(a,c,n/2)) + (pow(10,n/2)*(getKaratsuba(a,d,n/2)+getKaratsuba(b,c,n/2))) + getKaratsuba(b,d,n/2);
+    long int s1=getKaratsuba(a,c,n/2);
+    long int s2=getKaratsuba(b,d,n/2);
+    long int s3=getKaratsuba((a+b),(c+d),n/2);
+
+    long int res=s3-s2-s1;
+
+    long int product=(pow(10,n)*s1) + pow(10,n/2)*res + s2;
+    //long int product=(pow(10,n)*getKaratsuba(a,c,n/2)) + (pow(10,n/2)*(getKaratsuba(a,d,n/2)+getKaratsuba(b,c,n/2))) + getKaratsuba(b,d,n/2);
     return product;
 }
 
