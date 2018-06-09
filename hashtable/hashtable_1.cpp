6a1d416d43f76f5095509b94544bc0e9fc721afe
abrarShariar <shariarabrar@gmail.com>
2018-06-09 18:28:44 +0600

    hash table insert

---
+++ hashtable/hashtable_1.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+using namespace std;
+
+//hashnode class
+template<typename K, typename V>
+class HashNode {
+    public:
+        K key;
+        V value;
+
+    HashNode(K key, V value) {
+        this->key = key;
+        this->value = value;
+    }
+};
+
+//hashmap class
+template<typename K, typename V>
+class HashMap {
+
+    private:
+        HashNode<K,V> **arr;
+        int capacity;
+        int tableSize;
+        //dummy node
+        HashNode<K,V> *dummy;
+
+    public:
+        HashMap(int capacity){
+            this->capacity = capacity;
+            this->tableSize = 0;
+            this->arr = new HashNode<K,V>*[capacity];
+            this->dummy = new HashNode<K,V>(-1,-1);
+        }
+
+        //hash function
+        int hashCode(K key){
+            return key%this->capacity;
+        }
+
+        //insert new Node
+        void insertNode(K key, V value) {
+            HashNode<K, V> *temp = new HashNode<K, V>(key, value);
+            int hashIndex = this->hashCode(key);
+
+            while(arr[hashIndex] != NULL && arr[hashIndex]->key != -1 && arr[hashIndex]->key != key){
+                hashIndex++;
+                hashIndex %= this->capacity;
+            }
+
+            if(this->arr[hashIndex] == NULL && this->arr[hashIndex]->key == -1) {
+               this->tableSize++;
+               this->arr[hashIndex] = temp;
+            }
+        }
+
+
+        void display(){
+            for(int i = 0;i < this->capacity;i++){
+                if(arr[i] != NULL && arr[i]->key != -1){
+                    cout<<" Index: " << i;
+                    cout<<" Key: " << this->arr[i]->key;
+                    cout<<" Value: "<< this->arr[i]->value<<endl;
+                }
+            }
+        }
+};
+
+
+
+int main(){
+    int capacity = 20;
+    HashMap<int,int> *myHashMap = new HashMap<int, int>(capacity);
+    myHashMap->insertNode(100,0);
+    myHashMap->insertNode(200,1);
+    myHashMap->insertNode(500,2);
+
+    //myHashMap->display();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
